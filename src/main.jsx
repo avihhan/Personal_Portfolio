5127a5cff68d95783b9cad7401801bee59947d65
Avihhan <avihhan.official@gmail.com>
2025-09-24 15:37:14 -0400

    added analytics

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { Analytics } from '@vercel/analytics/react'
 
 // Register service worker for caching
 if ('serviceWorker' in navigator) {
@@ -19,5 +20,6 @@ if ('serviceWorker' in navigator) {
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
+    <Analytics />
   </React.StrictMode>,
 )
